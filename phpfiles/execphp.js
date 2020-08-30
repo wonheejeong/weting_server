46989baa1bad39514563b7cb0aabcd3a34bc6bab
eunohh <dmsdh316@naver.com>
2020-08-30 17:52:37 +0900

    gitignore 수정

---
+++ phpfiles/execphp.js
@@ -0,0 +1,31 @@
+
+/**
+*
+*/
+class ExecPHP {
+	/**
+	*
+	*/
+	constructor() {
+        this.phpPath = 'php.exe';
+		this.phpFolder = '';
+	}	
+	/**
+	*
+	*/
+	parseFile(fileName,callback) {
+		var realFileName = this.phpFolder + fileName;
+		
+		//console.log('parsing file: ' + realFileName);
+
+		var exec = require('child_process').exec;
+		var cmd = this.phpPath + ' ' + realFileName;
+		
+		exec(cmd, function(error, stdout, stderr) {
+			callback(stdout);
+		});
+	}
+}
+module.exports = function() {
+	return new ExecPHP();
+};
