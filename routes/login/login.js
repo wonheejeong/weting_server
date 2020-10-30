a1b26fa2ccc8cd267d4adf418c6c58a38c9da09e
eunohh <dmsdh316@naver.com>
2020-10-30 13:14:59 +0900

    socket 연결 테스트

--- routes/login/login.js
+++ routes/login/login.js
@@ -7,10 +7,26 @@ module.exports = function(app, connection)
     app.get('/', (req, res) => {      // 1
         console.log('get /');
             if(req.session.logined) {
-                res.send({
-                    'state': 200,
-                    'message': '로그인 상태'
+                io.once('connection', (socket) => {
+                    var name = '익명';
+                    socket.name = name;
+                    console.log('socket ' + socket.name + ' connected');
+        
+                    //socket 연결 해제
+                    socket.on('disconnect', () => {
+                        console.log('user ' + socket.name +  ' leaved');
+                    });
+                  
+                    //메시지 전송
+                    socket.on('send message', (name, text) => {
+                        var msg = name + ' : ' + text;
+                        io.emit('receive message', msg);
+                    });
                 });
+                // res.send({
+                //     'state': 200,
+                //     'message': '로그인 상태'
+                // });
             } else {
                 res.send({
                     'state':400,
