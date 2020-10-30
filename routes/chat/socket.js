5d024dc139908c2119164d07987fe05cca136e1c
eunohh <dmsdh316@naver.com>
2020-10-30 16:34:10 +0900

    socket 시험

--- routes/chat/socket.js
+++ routes/chat/socket.js
@@ -1,20 +1,19 @@
 module.exports = function(io, app, connection) {
-    app.get('/chats/66/:user_nick_name', (req, res) => {
-        var user_nick_name = req.params.user_nick_name;
+    app.get('/chats/66/hh', (req, res) => {
+        //var user_nick_name = req.params.user_nick_name;
 
         io.once('connection', (socket) => {
-            socket.name = user_nick_name;
-            console.log('user ' + socket.name + ' connected');
-            var chats = '새로운 멤버 ' + socket.name + '이/가 입장했습니다.\n';
-            io.emit('new user', chats, user_nick_name);
+            //socket.name = user_nick_name;
+            console.log('user  connected');
+            var chats = '새로운 멤버 ' + 'hh' + '이/가 입장했습니다.\n';
+            io.emit('new user', chats, 'hh');
 
             socket.on('disconnect', () => {
-                console.log('user ' + socket.name + ' disconnected');
+                console.log('user disconnected');
             });
             
-            socket.on('send message', (user_nick_name, text) => {
-                socket.name = user_nick_name;
-                var msg = socket.name + ' : ' + text;
+            socket.on('send message', (text) => {
+                var msg = hh + ' : ' + text;
                 io.emit('receive message', msg);
             });
         });
