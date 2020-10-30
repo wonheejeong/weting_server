231e60cbc6c991600a3b2deb2ea8f12df62c78d2
eunohh <dmsdh316@naver.com>
2020-10-30 19:39:54 +0900

    오타 수정

--- routes/chat/socket.js
+++ routes/chat/socket.js
@@ -2,7 +2,7 @@ module.exports = function(io, app) {
     app.get('/chats/66/:user_nick_name', (req, res) => {
         var name = req.params.user_nick_name;
 
-        io.on('connection', function(socket){ 
+        io.once('connection', function(socket){ 
             socket.name = name;
             console.log('user connected: ', socket.id);                  
             io.to(socket.id).emit('create name', name);   
