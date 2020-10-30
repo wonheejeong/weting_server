0ac75e79301cb36f167ec4e0467ca943b77e2222
eunohh <dmsdh316@naver.com>
2020-10-30 16:41:32 +0900

    채팅방 수정

--- routes/chat/socket.js
+++ routes/chat/socket.js
@@ -1,19 +1,18 @@
-module.exports = function(io, app, connection) {
+module.exports = function(io, app) {
     app.get('/chats/66/hh', (req, res) => {
-        //var user_nick_name = req.params.user_nick_name;
+        var user_nick_name = req.params.user_nick_name;
 
         io.once('connection', (socket) => {
-            //socket.name = user_nick_name;
             console.log('user  connected');
-            var chats = '새로운 멤버 ' + 'hh' + '이/가 입장했습니다.\n';
-            io.emit('new user', chats, 'hh');
+            var chats = '새로운 멤버 ' + user_nick_name + ' 이/가 입장했습니다.\n';
+            io.emit('new user', chats, user_nick_name);
 
             socket.on('disconnect', () => {
                 console.log('user disconnected');
             });
             
-            socket.on('send message', (text) => {
-                var msg = hh + ' : ' + text;
+            socket.on('send message', (user_nick_name, text) => {
+                var msg = user_nick_name + ' : ' + text;
                 io.emit('receive message', msg);
             });
         });
